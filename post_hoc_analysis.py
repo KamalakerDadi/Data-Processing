206c45362e4903f70d60eb5e369e6bc791d07d0f
KamalakerDadi <dkamalakarreddy@gmail.com>
2016-10-18 13:32:11 +0200

    function for analyze effects size

--- post_hoc_analysis.py
+++ post_hoc_analysis.py
@@ -4,7 +4,7 @@
 import collections
 import pandas as pd
 
-import statsmodels.formula.api as smf
+from statsmodels.formula.api import ols, mixedlm
 
 from nilearn._utils.compat import _basestring
 
@@ -47,3 +47,44 @@ def _categorize_data(data, columns):
     data = pd.DataFrame(data_dict)
 
     return data
+
+
+def analyze_effects(data, formula, model='ols'):
+    """Measure the effects size of each categorical variables given in
+    formula against dependent variable.
+
+    Uses module smf.ols from stats model (statistics in Python)
+
+    Parameters
+    ----------
+    data : pandas data frame
+
+    formula : str
+        Formula used in the specified model to fit model to the data.
+        See documentation of statsmodels.formula.api or related examples.
+
+    model : str, {'ols', 'mixedlm'}
+        Imported from statsmodels.formula.api
+
+    Returns
+    -------
+    model : instance of stats model whether 'ols' or 'mixedlm'
+        fit() of the model on the data.
+        print(model.summary()) to look at the summary of the fit() on data.
+        params can be fetched as model.params
+        pvalues can be fetched as model.pvalues
+    """
+    if not isinstance(data, pd.DataFrame):
+        raise ValueError("Given input 'data' should be like pandas Data frame."
+                         " You provided {0}".format(data))
+
+    if model not in ['ols', 'mixedlm']:
+        raise ValueError("model={0} you specified is not implemented. "
+                         "Choose between 'ols' or 'mixedlm'".format(model))
+
+    if model == 'ols':
+        model_fit = ols(formula=formula, data=data).fit()
+    elif model == 'mixedlm':
+        model_fit = mixedlm(formula=formula, data=data).fit()
+
+    return model_fit
