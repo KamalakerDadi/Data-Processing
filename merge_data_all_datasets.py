2fd827baf354637794a55e0721106a029b775968
KamalakerDadi <dkamalakarreddy@gmail.com>
2016-10-23 18:46:03 +0200

    Script to merge all datasets scores with column name as dataset

---
+++ merge_data_all_datasets.py
@@ -0,0 +1,31 @@
+"""
+"""
+import glob
+
+import pandas as pd
+
+from post_hoc_analysis import _categorize_data
+
+path_to_csvs = glob.glob('results_csv/*.csv')
+data_list = []
+
+for csv in path_to_csvs:
+    data_name = csv.split('/')[1].split('_')[1].split('.csv')[0]
+    print(data_name)
+    data = pd.read_csv(csv)
+
+    scores = data['scores'].str.strip('[ ]')
+    del data['scores']
+    data = data.join(scores)
+    data.scores = data.scores.astype(float)
+
+    # Categorize data with columns to pandas data frame
+    columns = ['scores', 'atlas', 'measure', 'classifier']
+    data = _categorize_data(data, columns=columns)
+
+    data['dataset'] = pd.Series([data_name] * len(data), index=data.index)
+
+    data_list.append(data)
+
+
+data = pd.concat(data_list)
