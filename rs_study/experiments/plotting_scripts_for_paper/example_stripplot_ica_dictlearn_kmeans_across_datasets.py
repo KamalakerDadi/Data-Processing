65d75bb68a6eee5d607a097a30dd98c33f22f5f7
KamalakerDadi <dkamalakarreddy@gmail.com>
2017-02-28 16:01:59 +0100

    Combine datasets with ICA, DICTLearn, KMeans for striplot

---
+++ rs_study/experiments/plotting_scripts_for_paper/example_stripplot_ica_dictlearn_kmeans_across_datasets.py
@@ -0,0 +1,77 @@
+"""Example script to run strip plot comparisons and saving them to pdf
+"""
+import os
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import load_data
+from my_palette import atlas_palette
+
+covariance_estimator = 'LedoitWolf'
+dataset_paths = dict()
+dataset_names = ['COBRE', 'ADNI']
+
+base_path = os.path.join('../prediction_scores', covariance_estimator)
+
+for dataset in dataset_names:
+    path = os.path.join(base_path, dataset)
+    path_ica = path + '/ICA/region_extraction/scores_ica.csv'
+    path_dict = path + '/DictLearn/region_extraction/scores_dictlearn.csv'
+    path_kmeans = path + '/KMeans/region_extraction/scores_kmeans_120.csv'
+    paths = [path_ica, path_dict, path_kmeans]
+    dataset_paths[dataset] = paths
+
+data_list = []
+for name in dataset_names:
+    this_data_path = dataset_paths[name]
+    this_data = load_data._pandas_data_frame_list_of_paths_concat(this_data_path)
+    data_list.append(this_data)
+
+data = pd.concat(data_list)
+data = data.drop('Unnamed: 0', axis=1)
+
+sns.set(color_codes=True)
+sns.set_style("whitegrid", {'axes.edgecolor': '.6', 'grid.color': '.6'})
+sns.set_palette('dark')
+
+scatter_kws = {'s': 5}
+line_kws = {'lw': 2}
+
+
+fig, axes = plt.subplots(nrows=1, ncols=len(dataset_names),
+                         figsize=(4, 3.5),
+                         squeeze=True, sharey=True)
+axes = axes.reshape(-1)
+
+NAMES = {'ica': 'ICA',
+         'dictlearn': 'DictLearn',
+         'kmeans': 'KMeans'}
+
+for i, (name, ax) in enumerate(zip(dataset_names, axes)):
+    for label in ['ica', 'dictlearn', 'kmeans']:
+        this_data = data[(data['dataset'] == name) &
+                         (data['atlas'] == label) &
+                         (data['classifier'] == 'svc_l2') &
+                         (data['measure'] == 'tangent')]
+        sns.stripplot(x='dimensionality', y='n_regions', data=this_data,
+                      ax=ax, jitter=.24, size=2.5, color=atlas_palette[label],
+                      )
+        if i == 0:
+            ax.set_ylabel('Number of regions extracted', size=15)
+        else:
+            ax.set_ylabel('')
+        ax.set_xlabel('')
+        plt.text(.5, 1.02, name, transform=ax.transAxes,
+                 size=13, ha='center')
+        for x in (1, 3):
+            ax.axvspan(x - .5, x + .5, color='.9', zorder=-1)
+
+plt.text(.6, .025, 'Number of components', transform=fig.transFigure,
+         size=15, ha='center')
+
+plt.tight_layout(rect=[0, .05, 1, 1])
+
+plt.savefig('n_regions_vs_dimensionality.pdf')
+plt.close()
+
